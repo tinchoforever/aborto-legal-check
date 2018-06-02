02ef499f92ab9df8c4d9cce0c7d5ccbee04b5d0f
Your Name <you@example.com>
2018-06-02 15:12:09 -0300

    mejoras

--- js/app.js
+++ js/app.js
@@ -12,3 +12,37 @@ angular.module('initApp',['ngRoute', 'ngAnimate'])
         controller: 'statsController'
       })
 });
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
--- js/check/main.js
+++ js/check/main.js
@@ -2,10 +2,54 @@
 
 angular.module('initApp')
   .controller('mainController', function ($scope) {
-    var url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRAP2fFHtZFimwpXco_Lub3Y0bAp5wNiVOx3Sp9p2gwGqnsihXZchD8aoMZDtIzbVkAQh4Z27Yfpss4/pub?output=csv';
+    var url = 'data.csv';
+
+    $scope.currentGame = {
+      points:0,
+      remaing: 5,
+    }
+    $scope.lastAnswer = {
+      result : false,
+    }
+    
+    $scope.finishGame = function(){
+      $scope.onQuestion = false;
+      $scope.onAnswer = false;
+      $scope.showResume = true;
+    }
+    $scope.nextQuestion = function(){
+      $scope.onQuestion = true;
+      $scope.onAnswer = false;
+      $scope.showResume = false;
+      $scope.currentCheck = $scope.currentGame.questions[$scope.currentGame.remaing-1];
+    }
+
+    $scope.responder = function(c){
+      var res = $scope.currentCheck['Resultado chequeo'];
+      $scope.lastAnswer.answer = c;
+      $scope.lastAnswer.result = c ===res;
+      if ($scope.lastAnswer.result){
+        $scope.currentGame.points++;
+      }
+      $scope.onQuestion =false;
+      $scope.onAnswer = true;
+     
+      $scope.currentGame.remaing--;
+       if ($scope.currentGame.remaing == 0){
+        $scope.gameFinish = true;
+      }
+    }
   	d3.csv(url, function(data){
       $scope.$apply(function(){
         $scope.checks = data;
+        var readyToCheck = [];
+        data.map(function(d){
+          if (d['Resultado chequeo'] != ''){
+            readyToCheck.push(d);
+          }
+        });
+        $scope.currentGame.questions = shuffle(readyToCheck).slice(0,5);
+       
       });
     });
 
